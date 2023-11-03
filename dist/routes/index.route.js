7879273c0febe6f51e4214f98e1e97e89233a5cf
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-11-03 21:58:27 +0200

    Build Application with added TODOs

--- dist/routes/index.route.js
+++ dist/routes/index.route.js
@@ -1,8 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const photo_model_1 = require("../models/photo.model");
 const router = (0, express_1.Router)();
-router.get('/', (req, res, next) => {
-    res.render('index');
+router.get('/', async (req, res, next) => {
+    try {
+        const photos = await photo_model_1.Photo.find();
+        /** @TODO Render Existing Photos in index.ejs */
+        res.render('index', { photos });
+    }
+    catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+router.post('/', 
+/** @TODO Add multer middleware */
+async (req, res, next) => {
+    try {
+        /** @TODO Save Photo in database */
+        res.redirect('/');
+    }
+    catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
 exports.default = router;
