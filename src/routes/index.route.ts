c953b34d434162d4dfbba8875a1738b77ff74b68
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-11-03 21:43:53 +0200

    Add TODOs to starter source code

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,9 +1,35 @@
 import { Router } from 'express';
+import { Photo } from '../models/photo.model';
+import multer from 'multer';
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
-    res.render('index');
+router.get('/', async (req, res, next) => {
+    try {
+        const photos = await Photo.find();
+    
+        /** @TODO Render Existing Photos in index.ejs */
+        res.render('index', { photos });
+    } catch (err) {
+        console.error(err);
+        next(err)
+    }
 })
+
+router.post(
+    '/',
+    /** @TODO Add multer middleware */
+    async (req, res, next) => {
+        try {
+
+            /** @TODO Save Photo in database */
+            res.redirect('/');
+
+        } catch (err) {
+            console.error(err);
+            next(err)
+        }
+    }
+)
 
 export default router;
